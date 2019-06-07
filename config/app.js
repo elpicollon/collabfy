e39c35ff48195555d3a80fe8083f5e61136e6ac4
Giovani de Oliveira <giovani.oliveira@rpinfo.com.br>
2019-06-07 16:31:59 -0300

    Changes for Heroku

--- config/app.js
+++ config/app.js
@@ -1,6 +1,3 @@
 module.exports = {
-  HOST: 'http://10.1.16.66:3000',
-  BOT_NAME: 'R2-D2'
-  // HOST: 'http://localhost:3000'
-  // HOST: 'http://192.168.5.101:3000'
+  HOST: 'https://collabfy.herokuapp.com/'
 };
